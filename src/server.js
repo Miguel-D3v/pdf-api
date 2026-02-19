ff9516266205e136bfdac3b07f01a1ad12721bdf
Miguel-D3v <miguelmodesto00@gmail.com>
2026-02-19 16:37:37 -0300

    set server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 import { PORT } from './config/env.js';
 import app from './app.js';
+import fs from "fs/promises";
+
+await fs.mkdir("uploads", { recursive: true });
+await fs.mkdir("output", { recursive: true });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
